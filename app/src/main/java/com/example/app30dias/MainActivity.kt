6539be0c179885e0ee642a480a72037a1fee26dc
JsRkll132 <jose.apaza2@unmsm.edu.pe>
2024-05-03 12:54:12 -0500

    80 percent made

--- app/src/main/java/com/example/app30dias/MainActivity.kt
+++ app/src/main/java/com/example/app30dias/MainActivity.kt
@@ -15,12 +15,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
+
+
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -35,6 +40,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -42,13 +48,18 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.app30dias.Data.DaysData
 import com.example.app30dias.Data.datList
 import com.example.app30dias.ui.theme.App30diasTheme
 import com.example.compose.backgroundLight
+import com.example.compose.backgroundLightHighContrast
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -104,17 +115,30 @@ fun listDays(listdats : List<DaysData>, modifier: Modifier = Modifier){
 @Composable
 fun DayCard(daycart: DaysData, modifier: Modifier = Modifier){
     val expandedState = remember { mutableStateOf(false) }
-    Card(modifier= modifier
+    Card(
+        modifier= modifier
         .graphicsLayer(scaleY = animateFloatAsState(if (expandedState.value) 1.1f else 1f).value)) {
         Column {
-            Row {
-                Text(text = LocalContext.current.getString(daycart.tittle))
-                Text(text = daycart.number_day.toString())
+            Row(modifier = Modifier
+                .align(Alignment.Start)
+                .padding(start = 10.dp, bottom = 5.dp, top = 8.dp)){
+                Text(text = LocalContext.current.getString(daycart.tittle),
+                    fontFamily = FontFamily(Font(R.font.blackhansans_regular),
+                        Font(R.font.poetsenone_regular)),
+                  )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = daycart.number_day.toString(),
+                    fontFamily = FontFamily(Font(R.font.blackhansans_regular),
+                        Font(R.font.poetsenone_regular)))
             }
 
-            Text(text = LocalContext.current.getString(daycart.name))
+            Text(text = LocalContext.current.getString(daycart.name),
+                fontFamily = FontFamily(Font(R.font.blackhansans_regular, FontWeight.ExtraBold),
+                    ),modifier = Modifier
+                    .padding(start = 10.dp)
+                    .align(Alignment.Start))
 
-
+            Spacer(modifier = Modifier.height(15.dp))
             Box(modifier = Modifier.clickable { expandedState.value = !expandedState.value }) {
                 Image(
                     painter = painterResource(id = daycart.pic),
@@ -130,11 +154,16 @@ fun DayCard(daycart: DaysData, modifier: Modifier = Modifier){
                 ) {
                 Text(
                     text = LocalContext.current.getString(daycart.content),
-                    modifier = Modifier.padding(top = 8.dp) // Añadir un espacio arriba para separar el texto de la imagen
+                    modifier = Modifier
+                        .padding(start = 10.dp, bottom = 8.dp)
+                        .align(Alignment.Start) ,
+                    fontFamily = FontFamily(Font(R.font.poetsenone_regular, FontWeight.ExtraBold),
+                    )// Añadir un espacio arriba para separar el texto de la imagen
                 )
             }
         }
     }
+    Spacer(modifier = Modifier.height(26.dp))
 }
 
 
